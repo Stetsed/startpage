e10a95d28d0c4b436d83d383adf5ed3693e9401b
Stetsed <stetsed@protonmail.com>
2023-05-18 15:37:24 +0200

    Made it catppuccin theme

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,9 +19,9 @@
 </script>
 
 <div
-  class="font-jetbrains h-screen bg-base-300 flex justify-center items-center"
+  class="font-jetbrains h-screen bg-background flex justify-center items-center"
 >
-  <div class="bg-base-300 rounded-lg shadow px-5 py-4 w-1/2">
+  <div class="bg-background rounded-lg shadow px-5 py-4 w-1/2">
     <div class="w-full mx-auto">
       <div class="flex items-center border-b py-2">
         <input
@@ -38,27 +38,25 @@
       </div>
     </div>
     <div class="h-5" />
-    <div class="flex justify-center text-center space-x-10 bg-base-300">
-      <div class="w-1/3 rounded-lg bg-base-200 p-4">
-        <div class="text-red-500 p-2 underline underline-offset-3">
+    <div class="flex justify-center text-center space-x-10 bg-background">
+      <div class="w-1/3 rounded-lg bg-boxbackground p-4">
+        <div class="text-ruby p-2 underline underline-offset-3">
           Social Media
         </div>
         <div><a href="https://www.reddit.com">Reddit</a></div>
         <div><a href="https://youtube.com">YouTube</a></div>
         <div><a href="https://discord.com">Discord</a></div>
       </div>
-      <div class="w-1/3 rounded-lg bg-base-200 p-4">
-        <div class="text-blue-800 p-2 underline underline-offset-3">
+      <div class="w-1/3 rounded-lg bg-boxbackground p-4">
+        <div class="text-sapphire p-2 underline underline-offset-3">
           Developing
         </div>
         <div><a href="https://github.com">GitHub</a></div>
         <div><a href="https://stackoverflow.com">Stack Overflow</a></div>
         <div><a href="https://gitlab.com">GitLab</a></div>
       </div>
-      <div class="w-1/3 rounded-lg bg-base-200 p-4">
-        <div class="text-yellow-500 p-2 underline underline-offset-3">
-          Extra
-        </div>
+      <div class="w-1/3 rounded-lg bg-boxbackground p-4">
+        <div class="text-emerald p-2 underline underline-offset-3">Extra</div>
         <div><a href="https://duckduckgo.com">DuckDuckGo</a></div>
         <div><a href="https://mail.proton.me">ProtonMail</a></div>
         <div><a href="https://chat.openai.com">ChatGPT</a></div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    colors: {
+      'background': "#1e1e2e",
+      'colortext': "#cdd6f4",
+      'sapphire': '#74c7ec',
+      'ruby': "#f38ba8",
+      'emerald': "#a6e3a1",
+      'boxbackground': '#313244',
+    },
     extend: {
       fontFamily: {
         jetbrains: ['JetBrains Mono', 'monospace'],
